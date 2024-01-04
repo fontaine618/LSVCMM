1fc39e777bb7af33b086f3e6c33642fa2ac7276e
fontaine618 <simfont@umich.edu>
2024-01-04 00:07:29 -0500

    updated conf_band to now allow simultaneaous across multiple VCs

--- R/conf_band.R
+++ R/conf_band.R
@@ -1,45 +1,54 @@
+#' @importFrom dplyr bind_rows
 confidence_band = function(
     obj,
     level=0.95,
     method=c("quantile"),
     var=2
 ){
-  if(!("LSVCMM.Boot" %in% class(obj))) stop("obj must be of class LSVCMM.Boot, i.e., the outpute of a lsvcmm.boot call")
+  if(!("LSVCMM.Boot" %in% class(obj))) stop("obj must be of class LSVCMM.Boot, i.e., the output of a lsvcmm.boot call")
   stopifnot(0<level & level<1)
   level = 1-level
   method = match.arg(method)
-  samples = obj$vc_boot[var,,,drop=F]  # 1 x nt x B
+  samples = obj$vc_boot[var,,,drop=F]  # nvars x nt x B
 
   out = switch(
     method,
     "quantile"=quantile_confidence_band(samples, level)
   )
 
-  pval = pointwise_pvalues(samples[1,,])
-  out = data.frame(
-    L=out$L,
-    U=out$U,
-    pointwise_confidence=1-2*out$p,
-    estimate=obj$vc[var, ],
-    mean=apply(samples, 2, mean),
-    median=apply(samples, 2, stats::median),
-    prop0=apply(samples, 2, function(x) mean(x==0)),
-    estimated_time=obj$unscaled_time,
-    level=proportion_samples_in_band(out$L, out$U, samples),
-    excludes_zero=(out$L>0) | (out$U<0),
-    pval=pval,
-    pval.adj=stats::p.adjust(pval, method="BH")
-  )
+  prop_in_band = proportion_samples_in_band(out$L, out$U, samples)
+  pval = pointwise_pvalues(samples)
+  nvars = length(var)
+  nt = dim(samples)[2]
+  pval.adj = matrix(stats::p.adjust(pval, method="BH"), nvars, nt)
+  df = lapply(seq_along(var), function(j){
+    data.frame(
+      L=out$L[j,],
+      U=out$U[j,],
+      pointwise_confidence=1-2*out$p,
+      estimate=obj$vc[j, ],
+      mean=apply(samples[j,,], 1, mean),
+      median=apply(samples[j,,], 1, stats::median),
+      prop0=apply(samples[j,,], 1, function(x) mean(x==0)),
+      estimated_time=obj$unscaled_time,
+      level=prop_in_band,
+      excludes_zero=(out$L[j,]>0) | (out$U[j,]<0),
+      pval=pval[j,],
+      pval.adj=pval.adj[j,],
+      var=var[j]
+    )
+  }) %>% dplyr::bind_rows()
 
-  return(out)
+  return(df)
 }
-
 
 #' @importFrom pracma logseq
 quantile_confidence_band = function(samples, level){
+  B = dim(samples)[3]
   nc = dim(samples)[2] * dim(samples)[1]
   minp = level / (2*nc)
   maxp = level / 2
+  # ps = c(seq(minp, maxp, 1/(B+1)), maxp)
   ps = pracma::logseq(minp, maxp, 200)
   prop = sapply(ps, function(p){
     out = pointwise_quantile_confidence_band(p, samples)
@@ -55,22 +64,31 @@ quantile_confidence_band = function(samples, level){
 
 pointwise_quantile_confidence_band = function(p, samples){
   qs = apply(samples, 1:2, stats::quantile, c(p, 1-p)) # 2 x len(var) x nt
-  L = qs[1,,] # NB this may be a vector or a matrix, cannot use drop=F ...
+  L = qs[1,,]
   U = qs[2,,]
+  if(dim(samples)[1]==1){
+    nt = dim(samples)[2]
+    L = matrix(L, 1, nt)
+    U = matrix(U, 1, nt)
+  }
   return(list(L=L, U=U, p=p))
 }
 
 proportion_samples_in_band = function(L, U, samples){
-  aL = apply(samples, 3, function(x) all(x>=L))
-  bU = apply(samples, 3, function(x) all(x<=U))
-  between = aL*bU
+  nvars = dim(samples)[1]
+  between = sapply(seq(nvars), function(j){
+    aL = apply(samples[j,,], 2, function(x) all(x>=L[j, ]))
+    bU = apply(samples[j,,], 2, function(x) all(x<=U[j, ]))
+    between = aL*bU
+  })
+  between = apply(between, 1, min)
   return(mean(between))
 }
 
 pointwise_pvalues = function(samples){
-  B = dim(samples)[2]+1
-  pos = apply(samples, 1, function(x) sum(x>0))+1
-  neg = apply(samples, 1, function(x) sum(x<0))+1
+  B = dim(samples)[3]+1
+  pos = apply(samples, 1:2, function(x) sum(x>0))+1
+  neg = apply(samples, 1:2, function(x) sum(x<0))+1
   ppos = 1-pos/B
   pneg = 1-neg/B
   pval_npm = pmin(2 * pmin(ppos, pneg), 1)
